45fdaa0ef3790dc0bbf3e9582f980297d1d96ec1
nisamozr <codeexplorkl10@gmail.com>
2021-10-13 21:17:19 +0530

    api setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const request = require('request')
+
 const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub')
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({blockchain})
+const Defalt_Port = 3000;
+const rootNodeAddress = `http://localhost:${Defalt_Port}`;
+// setTimeout(()=> pubsub.broadcastChain(),1000);
 
 app.use(bodyParser.json());
 
@@ -13,10 +20,35 @@ app.get('/api/blocks', (req, res)=>{
 app.post('/api/mine', (req, res)=>{
     const {data} = req.body;
     blockchain.addBlock({data});
+    pubsub.broadcastChain()
 
     res.redirect('/api/blocks');
 })
-const PORT = 3000;
+
+const syncChains = ()=>{
+    request({url: `${rootNodeAddress}/api/blocks`},(error, res, body)=>{
+        if(!error && res.statusCode === 200){
+            const rootChain = JSON.parse(body)
+
+            console.log('replace on a synce withw', rootChain)
+
+            blockchain.replaceChain(rootChain)
+        
+
+        }
+    })
+}
+
+let peerPort ;
+
+if(process.env.GENERTE_PEER_PORT === 'true'){
+    peerPort = Defalt_Port+ Math.ceil(Math.random()*1000)
+}
+const PORT = peerPort || Defalt_Port;
 app.listen(PORT, ()=>{
     console.log(`listing at localhost:${PORT}`)
+    if(PORT !== Defalt_Port){
+        syncChains();
+    }
+
 })
---
+++ pubnub.js
@@ -0,0 +1,13 @@
+const pubnub = require('pubnub')
+
+const credentials ={
+    publicKey: "pub-c-5f684895-f0f8-4dcf-9f5c-0dda3a31f9c9",
+    subscribeKey:  "sub-c-66a55f0a-2c11-11ec-bfec-fa2d187f6aa6",
+    secretKey : "sec-c-MThiOWY5YTUtYzE2Mi00ZWRhLThjYjUtNzNkZWNkMTQ3MTM1"
+}
+
+class Pubnub{
+    constructor(){
+
+    }
+}
