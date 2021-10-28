00dc34e1b48b1db41a8c98294621391a0d7156b5
nisamozr <codeexplorkl10@gmail.com>
2021-10-28 13:09:27 +0530

    evn set

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config()
+// require('dotenv').config()
 // const bodyParser = require('body-parser')
 const request = require('request')
 const path = require('path')
